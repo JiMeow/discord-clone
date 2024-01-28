325f81b5c3fe6cd5637d06e80f69506fb7d7290a
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2024-01-29 01:27:06 +0700

    Update authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div className="flex flex-col">This protected route</div>;
+  return (
+    <div>
+      K
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
